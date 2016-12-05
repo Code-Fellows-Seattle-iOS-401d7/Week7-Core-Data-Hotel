e0db0fa6c6195f0678f12845da874f301da17599
Filiz Kurban <filiz@monahq.com>
2016-12-04 20:52:48 -0800

    Week7HW4

---
+++ filizk/CoreDataHotelService/CoreDataHotelService/NSString+Common.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.h
+//  CoreDataHotelService
+//
+//  Created by Filiz Kurban on 12/4/16.
+//  Copyright © 2016 Filiz Kurban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Common)
+
+@end
---
+++ filizk/CoreDataHotelService/CoreDataHotelService/NSString+Common.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Common.m
+//  CoreDataHotelService
+//
+//  Created by Filiz Kurban on 12/4/16.
+//  Copyright © 2016 Filiz Kurban. All rights reserved.
+//
+
+#import "NSString+Common.h"
+
+@implementation NSString (Common)
+
+@end
